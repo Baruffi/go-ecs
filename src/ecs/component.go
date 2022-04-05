bee91c77894690e7986f00529c0ad2eed6a9714c
Raphael Antunes Baruffi <42071700+Baruffi@users.noreply.github.com>
2022-04-05 13:24:33 -0300

    Extended the api for scenes, registries, components and entities

--- src/ecs/component.go
+++ src/ecs/component.go
@@ -19,3 +19,25 @@ func NewComponent[C ComponentData]() Component[C] {
 		data: make(map[EntityId]C),
 	}
 }
+
+func (c Component[C]) GetId() ComponentId {
+	return c.id
+}
+
+func (c Component[C]) Has(e EntityId) bool {
+	_, ok := c.data[e]
+	return ok
+}
+
+func (c Component[C]) Get(e EntityId) (ComponentData, bool) {
+	d, ok := c.data[e]
+	return d, ok
+}
+
+func (c Component[C]) Set(e EntityId, d ComponentData) {
+	c.data[e] = d.(C)
+}
+
+func (c Component[C]) Unset(e EntityId) {
+	delete(c.data, e)
+}
