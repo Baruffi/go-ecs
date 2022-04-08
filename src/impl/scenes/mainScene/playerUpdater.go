bed7a17739504e1f07c3bd876368e59653f34889
Raphael Antunes Baruffi <42071700+Baruffi@users.noreply.github.com>
2022-04-08 11:52:28 -0300

    Lock camera to world map confines with colliders

--- src/impl/scenes/mainScene/playerUpdater.go
+++ src/impl/scenes/mainScene/playerUpdater.go
@@ -1,8 +1,6 @@
 package mainScene
 
 import (
-	"fmt"
-
 	"example.com/v0/src/ecs"
 	"example.com/v0/src/impl/components"
 	"github.com/faiface/pixel"
@@ -19,24 +17,17 @@ func (u *PlayerUpdater) Update(w *pixelgl.Window, dt float64) {
 	if camera, ok := ecs.GetComponent[*components.Combiner[*components.CameraComponent, *components.ColliderComponent]](u.Player); ok {
 		cameraComponent := camera.GetFirst()
 		cameraCollider := camera.GetSecond()
-
 		if cameraComponent.Active {
+			leftClickHeld := w.Pressed(pixelgl.MouseButtonLeft)
 			mousePosition := w.MousePosition()
 			mousePreviousPosition := w.MousePreviousPosition()
 			mouseScroll := w.MouseScroll()
 
-			if w.Pressed(pixelgl.MouseButtonLeft) {
-				mouseDelta := mousePosition.Sub(mousePreviousPosition)
-				cameraComponent.Move(mouseDelta)
-			} else {
-				cameraComponent.Move(pixel.ZV)
-			}
-			cameraComponent.Grow(mouseScroll.Y)
-			cameraComponent.Update(mousePosition)
+			previousArea := cameraCollider.Area
+			previousScale := cameraCollider.Scale
+			previousPos := cameraCollider.Pos
 
-			w.SetMatrix(cameraComponent.Matrix)
-
-			if w.Pressed(pixelgl.MouseButtonLeft) {
+			if leftClickHeld {
 				mouseDelta := mousePosition.Sub(mousePreviousPosition)
 				cameraCollider.Move(pixel.ZV.Sub(mouseDelta.Scaled(cameraCollider.Scale)))
 			} else {
@@ -46,15 +37,29 @@ func (u *PlayerUpdater) Update(w *pixelgl.Window, dt float64) {
 			cameraCollider.Grow(-mouseScroll.Y)
 			cameraCollider.Update(cameraComponent.Unproject(mousePosition))
 
-			if UICanvas, ok := ecs.GetComponent[*components.CanvasComponent](u.UI); ok {
-				UICanvas.InverseTransform(cameraComponent.Unproject(mousePosition), cameraComponent.DeltaPos, cameraComponent.DeltaScale, cameraComponent.Scale)
-			}
-		}
-		if worldMap, ok := ecs.GetComponent[*components.Combiner[*components.DrawComponent, *components.ColliderComponent]](u.World); ok {
-			worldMapCollider := worldMap.GetSecond()
+			if worldMap, ok := ecs.GetComponent[*components.Combiner[*components.DrawComponent, *components.ColliderComponent]](u.World); ok {
+				worldMapCollider := worldMap.GetSecond()
 
-			if !worldMapCollider.CollidesRect(cameraCollider.Area) {
-				fmt.Printf("OUT OF BOUNDS\n")
+				if worldMapCollider.CollidesVec(cameraCollider.Area.Min) && worldMapCollider.CollidesVec(cameraCollider.Area.Max) {
+					if leftClickHeld {
+						mouseDelta := mousePosition.Sub(mousePreviousPosition)
+						cameraComponent.Move(mouseDelta)
+					} else {
+						cameraComponent.Move(pixel.ZV)
+					}
+					cameraComponent.Grow(mouseScroll.Y)
+					cameraComponent.Update(mousePosition)
+
+					w.SetMatrix(cameraComponent.Matrix)
+
+					if UICanvas, ok := ecs.GetComponent[*components.CanvasComponent](u.UI); ok {
+						UICanvas.InverseTransform(cameraComponent.Unproject(mousePosition), cameraComponent.DeltaPos, cameraComponent.DeltaScale, cameraComponent.Scale)
+					}
+				} else {
+					cameraCollider.Area = previousArea
+					cameraCollider.Scale = previousScale
+					cameraCollider.Pos = previousPos
+				}
 			}
 		}
 	}
